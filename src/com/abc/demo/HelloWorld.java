aa6ec0c68e146644da9024113b378ac3a2e9e565
TorchXie <31877169+TorchXie@users.noreply.github.com>
2017-12-21 16:32:07 +0800

    Initial commit

---
+++ src/com/abc/demo/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.abc.demo;
+
+public class HelloWorld {
+    //程序的主函数入门
+    public static void main(String args[]) {
+        //输出打印字符语句
+        System.out.println("HelloWorld！");
+    }
+}
